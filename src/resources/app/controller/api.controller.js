191c624a62a42902cd17a55a758fe5e575ee81e8
HungRubi <huyhung180420022gmail.com>
2025-03-04 15:01:45 +0700

    test phamphuong

--- src/resources/app/controller/api.controller.js
+++ src/resources/app/controller/api.controller.js
@@ -2,7 +2,10 @@ const categoryProduct = require('../model/categoryProduct.model');
 const Product = require('../model/products.model')
 const Article = require('../model/article.model');
 const Banner = require('../model/banner.model');
+const { formatDate } = require('../../util/formatDate.util');
 class Api {
+
+    /** [GET] /api/home */
     getHome = async (req, res, next) => {
         try {
             const categories = [
@@ -35,6 +38,63 @@ class Api {
             next(err);
         }
     };
+
+    /** [GET] /api/product */
+    getProduct = async (req, res,next) => {
+        let page = parseInt(req.query.page) || 1
+        let limit = 12;
+        let skip = (page - 1) * limit;
+        try{
+            const [products] = await Promise.all([
+                Product.find()
+                .skip(skip)
+                .limit(limit)
+                .sort({ createdAt: -1 }),
+            ]);
+
+            const totalProduct = await Product.countDocuments();
+            const totalPage = Math.ceil(totalProduct / limit);
+            const data = { 
+                products,
+                currentPage: page,
+                totalPage,
+            };
+            res.json({data})
+        }catch(err){
+            next(err)
+        }
+    }
+
+    async getArticle(req, res, next) {
+        let page = parseInt(req.query.page) || 1;
+        let limit = 8;
+        let skip = (page - 1) * limit;
+        try {
+            const articles = await Article.find({ status: 'public' })
+                .skip(skip)
+                .limit(limit)
+                .sort({ createdAt: -1 })
+                .lean();
+    
+            const formatArticle = articles.map(article => ({
+                ...article,
+                dateFormat: formatDate(article.updatedAt)
+            }));
+    
+            const totalArticle = await Article.countDocuments();
+            const totalPage = Math.ceil(totalArticle / limit);
+            const data = {
+                articles: formatArticle,
+                currentPage: page,
+                totalPage,
+            };
+    
+            res.json({ data });
+        } catch (err) {
+            next(err);
+        }
+    }
+    
     
 }
 
--- src/resources/router/api.route.js
+++ src/resources/router/api.route.js
@@ -4,5 +4,7 @@ const route = express.Router();
 const apiController = require('../app/controller/api.controller');
 
 route.get('/home', apiController.getHome);
+route.get('/products', apiController.getProduct);
+route.get('/articles', apiController.getArticle);   
 
 module.exports = route;
