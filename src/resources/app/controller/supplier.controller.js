2c5dc461cbbbfd1553269f2b9eb6d58509e5ff3d
HungRubi <huyhung18042002@gmail.com>
2025-05-06 14:58:33 +0700

    test pc

---
+++ src/resources/app/controller/supplier.controller.js
@@ -0,0 +1,7 @@
+const Supplier = require('../model/supplier.model');
+
+class Supplier {
+    index (req, res) {
+
+    }
+}
---
+++ src/resources/app/model/supplier.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const supplier = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        phone: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            trim: true
+        },
+        address: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    },{
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("supplier", supplier)
